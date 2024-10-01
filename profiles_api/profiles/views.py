95801f19ad630c3de2b5bbb8c0be6220a1cd365e
Emmanuel Momoh <momohemmanuel370@gmail.com>
2024-10-01 15:54:28 +0100

    added post,put,patch and delete methods

--- profiles_api/profiles/views.py
+++ profiles_api/profiles/views.py
@@ -1,8 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles import serializers
 
 class HelloApiView(APIView):
     """Testing API view"""
+    serializer_class = serializers.HelloSerializer
     
     def get(self,request,format = None):
         """Returns a list of API view Features"""
@@ -14,3 +17,27 @@ class HelloApiView(APIView):
         ]
         
         return Response({'message':'Hello','an_apiview':an_apiview})
+    
+    def post(self,request,format = None):
+        """Create a hello message with name """
+        serializer = self.serializer_class(data = request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            return Response({'message':f'Hello {name}'})
+        else:
+            return Response(serializer.errors,
+                            status = status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self,request,pk = None):
+        """Handles updating an entire object """
+        return Response({'method':'PUT'})
+    
+    def patch(self,request,pk = None):
+        """Handles a partial update of an object """
+        return Response({'method':'PATCH'})
+    
+    
+    def delete(self,request,pk = None):
+        """Deletes an Object"""
+        return Response({'method':'DELETE'})
